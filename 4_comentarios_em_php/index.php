98df1cb04ecf51a2a4d75fa836c7302df0c976f2
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:24:53 -0300

    comentarios_em_php

---
+++ 4_comentarios_em_php/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Este é um comentário de uma linha
+echo "Teste de comentário <br>"; // Imprime o teste de comentário
+
+# Outro tipo de comentário
+echo "Teste de comentário <br>";
+
+/*
+Comentario de várias linhas
+*/
+
