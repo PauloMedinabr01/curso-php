ea466a173241b634a4dafab0e4fdc3da43c93074
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:35:49 -0300

    palavras_reservadas

---
+++ 6_palavras_reservadas/index.php
@@ -0,0 +1,16 @@
+<?php
+
+/* function echo(){ // echo é uma palavra reservada do php
+    return true;
+}
+
+function while(){   // while é uma palavra reservada do php
+    return true;
+}
+
+*/
+
+function whileNew()
+{
+    return true;
+}
