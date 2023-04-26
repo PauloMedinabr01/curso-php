d7dc84e60d5fcfa9e0bd5e4a5abfac0aff738ecc
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 12:54:08 -0300

    checando_float

---
+++ 3_tipos_de_dados/checando_float/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = "teste";
+$b = 12.8;
+$c = 6565.3;
+$d = "teste";
+
+
+//true
+if (is_float($a)) {
+    echo "a é um float <br>";
+}
+
+if (is_float($b)) {
+    echo "b é um float <br>";
+}
+
+if (is_float($c)) {
+    echo "c é um float <br>";
+}
+
+if (is_float($d)) {
+    echo "d é um float <br>";
+}
