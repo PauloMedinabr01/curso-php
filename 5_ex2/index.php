b9feca249d0235640849c90f720f600f91aae088
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:30:55 -0300

    ex2

---
+++ 5_ex2/index.php
@@ -0,0 +1,13 @@
+<?php
+
+
+// Variável nome contem o nome de uma pessoa
+$nome = "Paulo ";
+
+// Variável endereço contem o endereço de uma pessoa
+$endereco = "Rua, 1 ";
+
+// Variável telefone contem o telefone de uma pessoa
+$telefone = "999999999";
+
+echo $nome, $endereco, $telefone;
