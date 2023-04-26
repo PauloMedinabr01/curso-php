4632570a439ec8c0c551303358f91ed6b072067e
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:08:03 -0300

    case_sensitive

---
+++ 1_case_sensitive/index.php
@@ -0,0 +1,16 @@
+<?php
+
+//Não é case sensitive
+echo "teste <br>";
+
+eCho "testando 2 <br>";
+
+ECHO "testando 3 <br>";
+
+//É case sensitive
+$nome = "Paulo";
+$NOME = "Teste";
+
+echo $nome;
+echo "<br>";
+echo $NOME;
