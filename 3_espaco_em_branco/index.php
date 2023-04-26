f85ce3a785f359fb17600f685d03098c04c24d9a
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:20:03 -0300

    espaco_em_branco

---
+++ 3_espaco_em_branco/index.php
@@ -0,0 +1,16 @@
+<?php
+
+echo         "Testando espaços em branco";
+
+echo "Testando espaços em branco <br>"               ;
+
+echo         
+"Testando espaços em branco"
+;
+
+echo "Testando espaços em branco"
+;
+
+echo "Testando espaços 
+em branco";
+
