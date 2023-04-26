6300ec9ba476577abab00b9a899c685b4c6b0020
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 14:13:17 -0300

    checando_string

---
+++ checando_string/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$str = "Matheus";
+$num = 12;
+
+if (is_string($str)) {
+    echo "$str é String <br>";
+} else {
+    echo "$str não é String <br>";
+}
+
+if (is_string($num)) {
+    echo "$num é String <br>";
+} else {
+    echo "$num não é String <br>";
+}
