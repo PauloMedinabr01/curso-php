4ff2ff491a53cb1b116dfa0dd753052123bbadd1
Paulo Medina <paulomedinabr01@gmail.com>
2023-04-26 11:14:22 -0300

    instrucao_de_codigo

---
+++ instrucao_de_codigo/index.php
@@ -0,0 +1,12 @@
+<?php
+
+// Instrução de uma linha
+echo "Uma linha <br>";
+echo "Outra linha <br>";
+
+//Instrução de mais de uma linha
+if (5 > 7) {
+    echo "Maior";
+} else {
+    echo "Menor";
+}
