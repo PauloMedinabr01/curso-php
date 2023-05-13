79e38ff213d22afaeab8441b7a3de94302a8546d
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-13 12:22:52 -0300

    Ajustes

--- 3_tipos_de_dados/arrays/index.php
+++ 3_tipos_de_dados/arrays/index.php
@@ -4,10 +4,14 @@ $a = [1, 2, 3, 4, 5]; // array com números inteiros
 
 print_r($a); // imprimi o array
 echo "<br>";
+
 echo $a[2]; // imprimi o indice 2 do array
 echo "<br>";
+
 $arr = ["Paulo", 1, 2, '3', true]; // array com strings, números inteiros, caracter simples e boolean
 echo "<br>";
+
 print_r($arr); // imprimi o array
 echo "<br>";
+
 print_r($arr[0]); // imprimi o indice 0 do array
