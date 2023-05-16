690bc090e47e9d570b3f4ee7da2b8a433fa5566d
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-16 16:24:24 -0300

    ex6

---
+++ 3_tipos_de_dados/ex6/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$arr_carro = ['Carro' => 'Sedan', 'Cor' => 'Azul', 'Motor' => 1.4];
+
+print_r($arr_carro);
+echo "<br>";
+
+echo $arr_carro['Carro'];
+echo "<br>";
+
+echo $arr_carro['Cor'];
+echo "<br>";
+
+echo $arr_carro['Motor'];
