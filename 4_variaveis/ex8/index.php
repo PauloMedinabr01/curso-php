5b045d3e8e736f36855e8ba96c25baa983add9c3
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-16 22:24:11 -0300

    ex9

---
+++ 4_variaveis/ex8/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$carro = "Ferrari";
+$cor = "vermelho";
+$velocidade = 200.0;
+
+echo "Meu carro é uma $carro, da cor $cor e a velocidade chega aos $velocidade km/h.";
+echo "<br>";
+
+$arr1 = [1, 2, 3, 4, 5];
+print_r($arr1);
+echo "<br>";
+echo $arr1[0];
+echo "<br>";
+
+$arr2 = ['Modelo' => 'Ferrari', 'Cor' => 'Vermelho', 'Velocidade' => 200];
+print_r($arr2);
---
+++ 4_variaveis/ex9/index.php
@@ -0,0 +1,21 @@
+<?php
+
+$num1 = 2; // int
+$num2 = 4; // int
+$soma = $num1 + $num2; // soma de int
+$num3 = 112.0; // float
+
+echo $num1; // imprimi num1
+echo "<br>";
+
+echo $num2; //imprimi num2
+echo "<br>";
+
+echo $soma; // imprimi a soma de num1 e num2
+echo "<br>";
+
+echo $num3; //imprimi num3
+echo "<br>";
+
+echo $somaIntFloat = $num1 + $num2 + $num3; // imprimi a soma de num1, num2 e num3
+echo "<br>";
