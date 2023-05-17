6c2511a64d758f026bc9f52f8126d6c0792d877c
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-16 22:07:49 -0300

    criacao_de variaveis

---
+++ 4_variaveis/criacao_de_var/index.php
@@ -0,0 +1,17 @@
+<?php
+
+$teste = "Paulo";
+echo $teste;
+echo "<br>";
+
+$num = 10;
+echo $num;
+echo "<br>";
+
+$velocidadeMaxima = 100;
+echo $velocidadeMaxima;
+echo "<br>";
+
+$velocidade_minima = 100;
+echo $velocidade_minima;
+echo "<br>";
