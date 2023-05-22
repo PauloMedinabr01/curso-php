48b3e7dd22a0a46b3bd0778c4dc30f56cb4bdcf2
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-22 15:13:21 -0300

    ex10

---
+++ 4_variaveis/ex10/index.html.php
@@ -0,0 +1,18 @@
+<?php
+
+$varA = 2;
+$varB = 5;
+$varC = 10;
+$soma = $varA + $varB + $varC;
+$subtracao = $varA - $varB - $varC;
+$multiplicacao = $varA * $varB * $varC;
+$divisao = $soma / $varA;
+$operacao = $soma + $subtracao - $soma / $varA * $varB;
+
+
+echo "<br>";
+echo "Valor das variáveis: valor de A = $varA, valor de B = $varB, valor de C = $varC <br> ";
+echo "Valor da soma = $soma, valor da subtração = $subtracao, valor da divisão = $divisao <br> ";
+echo "O valor da operação é = $operacao <br>";
+
+
