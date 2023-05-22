6a2d78968524e04a83a644a949e253656cf13241
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-22 16:28:52 -0300

    operadores_aritmeticos

---
+++ 5_operadores_aritmeticos/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$soma = 5 + 5;
+$subtracao = 10 - 5;
+$multiplicacao = 2 * 2;
+$divisao = 20 / 4;
+
+echo "A soma é = $soma <br>";
+echo "A subtração é = $subtracao <br>";
+echo "A multiplicação é = $multiplicacao <br>";
+echo "A divisão é = $divisao <br>";
+
+$operacao = 5 + 5 - (3 / 2) * 5;
+echo "O valor da operação é = $operacao <br>";
