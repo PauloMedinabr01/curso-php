d9753b941e268b5625961d735f25637f3e511ce2
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-22 15:49:07 -0300

    ex11

---
+++ 4_variaveis/ex11/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$operacao = "5" * 12;
+
+echo $operacao . "<br>";
+
+echo gettype($operacao);
+echo "<br>";
+echo gettype([1, 2, 3, 4, 5]);
+echo "<br>";
+echo gettype(12.0);
+echo "<br>";
+echo gettype("Paulo");
+echo "<br>";
