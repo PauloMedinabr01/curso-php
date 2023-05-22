f90230ebb53dff8da2895950edcd4e9bcc6bcfed
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-22 16:38:45 -0300

    ex12

---
+++ 5_operadores_aritmeticos/ex12/index.php
@@ -0,0 +1,14 @@
+<?php
+
+# +, -, *, /
+
+$soma = 20 + 30;
+$subtracao = 200 - 139;
+$multiplicacao = 2 * 36;
+$divisao = 700 / 3;
+
+
+echo "O valor da soma é = $soma <br>";
+echo "O valor da subtração é = $subtracao <br>";
+echo "O valor da multiplicação é = $multiplicacao <br>";
+echo "O valor da divisão é = $divisao <br>";
