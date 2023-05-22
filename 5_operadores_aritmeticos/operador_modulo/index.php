1d8b3384e06ded2e34ac430f1c1b8fb55dafd0d5
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-22 16:51:04 -0300

    operador_modulo

---
+++ 5_operadores_aritmeticos/operador_modulo/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$modPar = 12 % 2;
+$divisao = 12 / 2;
+$modImpar = 13 % 2;
+$modImpar2 = 13 % 5;
+
+
+echo "Resultado de Módulo 12 % 2 = $modPar <br>";
+echo "Resultado de divisão 12 / 2 = $divisao <br>";
+echo "Resultado de Módulo 13 % 2 = $modImpar <br>";
+echo "Resultado de Módulo 13 % 5 = $modImpar2 <br>";
+
+
