39b4eabf39277410aab1a6744b6dca49922f963b
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-05-26 20:27:36 -0300

    op_concatenacao

---
+++ 5_operadores_aritmeticos/op_concatenacao/index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "testando a concatenação";
+echo "<br>";
+echo "testando" . " a" . " concatenação";
+echo "<br>";
+
+$a = "testando";
+$b = "a";
+$c = "concatenacao";
+
+echo $a . " " . $b . " " . $c;
+echo "<br>";
+
+$marca = "Ferrari";
+$motor = "3.0 V8";
+$velMaxima = 200;
+
+echo $marca . " " . $motor . " " . $velMaxima . " km/h";
