6902012e6195787d3a0131b8793229f979eed704
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-06-04 21:14:05 -0300

    operador_identico

---
+++ 5_operadores_aritmeticos/operador_identico/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$a = 5;
+$b = 5;
+
+// igualdade
+if (5 == "5") {
+    echo "5 é 5 1 <br>";
+}
+
+// op de identico
+if (5 === "5") {
+    echo "5 é identico a 5 2 <br>";
+}
+
+// op de identico
+if (5 === 5) {
+    echo "5 é identico a 5 2 <br>";
+}
+
