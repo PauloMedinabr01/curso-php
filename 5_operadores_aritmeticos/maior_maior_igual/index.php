c0a8ffee23647e40d98b040174a134e9d124fe61
PauloMedinabr01 <paulomedinabr01@gmail.com>
2023-06-04 21:46:45 -0300

    maior_maior_igual

---
+++ 5_operadores_aritmeticos/maior_maior_igual/index.php
@@ -0,0 +1,30 @@
+<?php
+
+$a = 4;
+$b = 5;
+$c = 6;
+$d = 6;
+
+if ($a > $b) {
+    echo "A é maior que B <br>";
+} else {
+    echo "A não é maior que B <br>";
+}
+
+if ($b > $a) {
+    echo "B é maior que A <br>";
+} else {
+    echo "B não é maior que A <br>";
+}
+
+if ($d >= $c) {
+    echo "D é maior ou igual que C <br>";
+} else {
+    echo "D não é maior ou igual a C <br>";
+}
+
+if ($d >= $a) {
+    echo "D é maior ou igual que A <br>";
+} else {
+    echo "D não é maior ou igual a A <br>";
+}
